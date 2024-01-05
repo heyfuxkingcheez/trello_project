bb7df5c734a97bf32f80cbd41cc7ec101f26594e
ProjectBYH <wnrhd45@gmail.com>
2024-01-05 19:56:01 +0900

    Feat: 회원가입 & 로그인 & 회원정보조회 추가
    - 회원 정보 수정 미구현
    - 회원 탈퇴 미구현
    - 로그인 구현
    - 엑세스 토큰 재발급 구현
    - 회원가입 구현
    - 회원정보조회 구현
    - Redis 리프레시 토큰 저장 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+// src/auth/auth.service.ts
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+import { RedisService } from '../configs/redis/redis.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+    private redisService: RedisService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(email);
+    if (!user || !(await bcrypt.compare(pass, user.password))) {
+      throw new UnauthorizedException(
+        '존재하지 않는 회원이거나 비밀번호가 틀립니다.',
+      );
+    }
+
+    return user;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    const accessToken = this.jwtService.sign(payload);
+
+    const refreshToken = this.jwtService.sign(payload, { expiresIn: '7d' });
+
+    await this.redisService.setRefreshToken(user.email, refreshToken);
+
+    return {
+      message: '로그인 완료',
+      access_token: accessToken,
+    };
+  }
+
+  generateAccessToken(user: any) {
+    const payload = { email: user.email, sub: user.id };
+
+    return this.jwtService.sign(payload);
+  }
+}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+// src/users/dto/create-user.dto.ts
+import { IsString, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  email: string;
+
+  @IsString()
+  username: string;
+
+  @IsString()
+  password: string;
+
+  @IsNumber()
+  @IsOptional()
+  imageUrl?: number;
+
+  @IsString()
+  @IsOptional()
+  comment?: string;
+}
