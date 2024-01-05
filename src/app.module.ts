aac7794251065e6ee15929ee19507cca5d61b2c8
woogi-on_and-on <jgw987@gmail.com>
2024-01-05 12:11:26 +0900

    feat [dev] nestjs | 환경변수 | typeORM 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModuleValidationSchema } from './configs/env-valid';
+import { typeOrmModuleOptions } from './configs/database-config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: ConfigModuleValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
