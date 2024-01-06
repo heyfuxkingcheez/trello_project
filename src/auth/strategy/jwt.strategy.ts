25ac9870ecfa33f8bf8cdb02d3ae2343a55c8b0d
ProjectBYH <wnrhd45@gmail.com>
2024-01-06 23:27:37 +0900

    Fix [feature/userCRUD] 토큰이 존재하지 않는 경우 에러 메세지 추가

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -22,7 +22,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   async validate(payload: any) {
     const user = await this.userService.findOne(payload.email);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('로그인이 필요합니다.');
     }
 
     // 토큰 만료 시간 추가
