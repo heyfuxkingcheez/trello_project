04700af2fbec2d87aab43d9067182387f6f9b222
Han9526 <jk2tmdwns@naver.com>
2024-01-08 18:27:07 +0900

    Refactor  [feature/columnCRUD] columnDTO 수정

--- src/columns/dto/column.dto.ts
+++ src/columns/dto/column.dto.ts
@@ -2,13 +2,7 @@ import { IsNotEmpty } from 'class-validator';
 import { PickType } from '@nestjs/mapped-types';
 import { CardColumn } from 'src/columns/entities/column.entity';
 
-export class CardColumnDto extends PickType(CardColumn, [
-  'name',
-  'orderIndex',
-]) {
+export class CardColumnDto extends PickType(CardColumn, ['name']) {
   @IsNotEmpty({ message: '컬럼명을 입력해 주세요' })
   name: string;
-
-  @IsNotEmpty({ message: 'Index를 입력해 주세요' })
-  orderIndex: number;
 }
