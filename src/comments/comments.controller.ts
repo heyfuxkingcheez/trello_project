33867e2998b073823b88fbbc0bd2bad59b1bb772
woogi-on_and-on <jgw987@gmail.com>
2024-01-09 13:54:35 +0900

    Feat [feature/commentCRUD] 댓글 crud 완료

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,76 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Card } from 'src/cards/entities/card.entity';
+import { Repository } from 'typeorm';
+import { Comment } from './entities/comment.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { CommentsService } from './comments.service';
 
-@Controller('comments')
-export class CommentsController {}
+@Controller('board')
+@UseGuards(AuthGuard('jwt'), JwtAuthGuard)
+export class CommentsController {
+  constructor(
+    @InjectRepository(Card)
+    private readonly cardRepository: Repository<Card>,
+    @InjectRepository(Comment)
+    private commentRepository: Repository<Comment>,
+    private commentsService: CommentsService,
+  ) {}
+  // 댓글 생성
+  @Post('/:boardId/card/:cardId')
+  async createCard(
+    @Param('boardId') boardId: string,
+    @Param('cardId') cardId: string,
+    @Body() createCommentDto: CreateCommentDto,
+    @Req() req,
+  ) {
+    const createComment = await this.commentsService.createComment(
+      req.user.id,
+      +boardId,
+      +cardId,
+      createCommentDto.text,
+    );
+
+    return { status: HttpStatus.OK, message: '등록 완료', createComment };
+  }
+
+  // 카드별 댓글 조회
+  @Get('/:boardId/card/:cardId')
+  async getComments(
+    @Param('boardId') boardId: string,
+    @Param('cardId') cardId: string,
+  ) {
+    const getComments = await this.commentsService.getComments(+cardId);
+
+    return { status: HttpStatus.OK, message: '조회 완료', getComments };
+  }
+
+  // 댓글 삭제
+  @Delete('/:boardId/card/:cardId/comment/:commentId')
+  async deleteComment(
+    @Param('boardId') boardId: string,
+    @Param('cardId') cardId: string,
+    @Param('commentId') commentId: string,
+    @Req() req,
+  ) {
+    const deleteComment = await this.commentsService.deleteComment(
+      +cardId,
+      req.user.id,
+      +commentId,
+    );
+
+    return { status: HttpStatus.OK, message: '삭제 완료', deleteComment };
+  }
+}
--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { Card } from 'src/cards/entities/card.entity';
+import { CardsModule } from 'src/cards/cards.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Comment, Card]), CardsModule],
   controllers: [CommentsController],
-  providers: [CommentsService]
+  providers: [CommentsService],
 })
 export class CommentsModule {}
