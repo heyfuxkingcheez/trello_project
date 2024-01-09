8425e80969274546eb6f6c5b6d9be917e6325624
asdfg20564 <44521546+asdfg20564@users.noreply.github.com>
2024-01-09 14:19:11 +0900

    feat [boardCRUD] board curd
    feat [boardCRUD] board curd

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { BoardsService } from './boards.service';
+import { boardDto } from './dto/board.dto';
+@UseGuards(AuthGuard('jwt'))
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+    constructor(private readonly boardService: BoardsService) {}
+
+    @Post()
+    async createBoard(@Body() boardDto: boardDto, @Req() req) {
+        const userId = req.user.id;
+        return this.boardService.createBoard(userId, boardDto);
+    }
+
+    @Get()
+    async GetAllBoard(@Req() req) {
+        const userId = req.user.id;
+        return this.boardService.getAllBoard(userId);
+    }
+
+    @Get(':id')
+    async GetBoard(@Param('id') boardId: number, @Req() req) {
+        const userId = req.user.id;
+        return this.boardService.getBoard(boardId, userId);
+    }
+
+    @Patch(':id')
+    async updateBoard(@Param('id') boardId: number, @Body() boardDto: boardDto, @Req() req) {
+        const userId = req.user.id;
+        return this.boardService.updateBoard(userId, boardDto, boardId);
+    }
+
+    @Delete(':id')
+    async deleteBoard(@Param('id') boardId: number, @Req() req) {
+        const userId = req.user.id;
+        return this.boardService.deleteBoard(userId, boardId);
+    }
+}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { Board } from './entities/board.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Board, User])],
   controllers: [BoardsController],
-  providers: [BoardsService]
+  providers: [BoardsService],
+  exports: [BoardsService],
 })
 export class BoardsModule {}
