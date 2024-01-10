ec681a4c6791d2c0d9e83fa296956d2703c49526
PachyuChepe <pachyuchepe@gmail.com>
2024-01-11 01:20:28 +0900

    Fix [feature/userCRUD] main.ts https 동작 & 깃 이그노어 인증서 추가 & package-lock.json 재설치

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import * as fs from 'fs';
+import * as https from 'https';
+import * as path from 'path';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
@@ -7,6 +11,25 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('SERVER_PORT');
-  await app.listen(port);
+  // 인증 키 파일 경로 설정
+  const keyPath = path.join(__dirname, '..', 'key.pem');
+  const certPath = path.join(__dirname, '..', 'cert.pem');
+
+  // HTTPS 설정
+  if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+    const httpsOptions = {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    };
+    await app.init();
+    https
+      .createServer(httpsOptions, app.getHttpAdapter().getInstance())
+      .listen(port);
+    console.log(`HTTPS server running on port ${port}`);
+  } else {
+    // HTTP로 실행
+    await app.listen(port);
+    console.log(`HTTP server running on port ${port}`);
+  }
 }
 bootstrap();
